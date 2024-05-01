3a950701fef093679e2feefc2731a6288c920bc3
“Kunal <kunalkoushik44@gmail.com>
2024-05-01 20:59:39 +0530

    commit

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,5 +1,4 @@
-// const bcrypt = require("bcrypt")
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcrypt")
 const User = require("../models/User")
 const OTP = require("../models/OTP")
 const jwt = require("jsonwebtoken")
